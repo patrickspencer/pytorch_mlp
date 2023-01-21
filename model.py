ae48c7216b07efb39989a3e5ed4d142e5af4d9bb
Patrick Spencer <pkspenc@gmail.com>
2023-01-20 21:41:58 -0500

    Add model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from torch import nn
+
+
+class MLP(nn.Module):
+    def __init__(self, D_in, H, D_out):
+        """
+        3 layer neural network with dropout and final sigmoid activation
+        """
+        super(MLP, self).__init__()
+        self.linear1 = nn.Linear(D_in, H)
+        self.linear2 = nn.Linear(H, D_out)
+        self.sigmoid = nn.Sigmoid()
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        """
+        Simple forward function
+        """
+        y0 = self.dropout(self.linear1(x).clamp(min=0))
+        y1 = self.sigmoid(self.linear2(y0))
+        return y1
